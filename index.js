f1d42a39cb3d483c31d29c32a519adc3d9fd65cb
Choco2206 <k.alvaro@gmx.de>
2026-06-08 23:47:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const { Client, GatewayIntentBits } = require("discord.js");
+const fs = require('fs');
+const path = require('path');
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers
-  ]
+    GatewayIntentBits.GuildMembers,
+  ],
 });
 
-client.once("clientReady", () => {
-  console.log(`✅ Bot online als ${client.user.tag}`);
-});
+// EVENTS LADEN
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
 
 client.login(process.env.DISCORD_TOKEN);
